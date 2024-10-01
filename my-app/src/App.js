2a26bef918e473c90e0f9320ca29d9e18542458f
fionntmcc <127796057+fionntmcc@users.noreply.github.com>
2024-10-01 21:37:25 +0100

    5) Add Navbar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,12 +7,14 @@
 import Header from './components/Header';
 import Content from './components/Content';
 import Footer from './components/Footer';
+import NavigationBar from './components/NavigationBar';
 import './App.css';
 
 function App() {
   
   return (
     <div>
+      <NavigationBar />
       <Header />
       <Content />
       <Footer />
